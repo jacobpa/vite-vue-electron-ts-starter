860e5916608315d14fa82285689d21d3d365d0de
Jacob Patterson <jacob@jacobpa.com>
2021-12-20 18:50:50 -0500

    chore: add main electron entry
    Config from https://github.com/electron-userland/electron-webpack-quick-start

---
+++ src/main/index.ts
@@ -0,0 +1,55 @@
+import { app, BrowserWindow } from "electron";
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+// global reference to mainWindow (necessary to prevent window from being garbage collected)
+let mainWindow: BrowserWindow | null;
+
+function createMainWindow() {
+  const window = new BrowserWindow({
+    webPreferences: { nodeIntegration: true },
+  });
+
+  if (isDevelopment) {
+    window.webContents.openDevTools();
+  }
+
+  if (isDevelopment) {
+    window.loadURL(`http://localhost:3000`);
+  } else {
+    window.loadFile("dist/render/index.html");
+  }
+
+  window.on("closed", () => {
+    mainWindow = null;
+  });
+
+  window.webContents.on("devtools-opened", () => {
+    window.focus();
+    setImmediate(() => {
+      window.focus();
+    });
+  });
+
+  return window;
+}
+
+// quit application when all windows are closed
+app.on("window-all-closed", () => {
+  // on macOS it is common for applications to stay open until the user explicitly quits
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  // on macOS it is common to re-create a window even after all windows have been closed
+  if (mainWindow === null) {
+    mainWindow = createMainWindow();
+  }
+});
+
+// create main BrowserWindow when electron is ready
+app.on("ready", () => {
+  mainWindow = createMainWindow();
+});
